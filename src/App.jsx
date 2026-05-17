2aeeb264372ebf7ebee3094a99585ca33a125d6e
mash-xf <felix.macharia@student.moringaschool.com>
2026-05-17 11:56:33 +0300

    test keys

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,13 @@ function App() {
 
 
   // const PUBLISHABLE_KEY = `pk_live_Y2xlcmsubWFzaC14Zi5naXRodWIuaW8k`
-  const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY || import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+  const PUBLISHABLE_KEY = "pk_test_YWRhcHRpbmctaGFnZmlzaC00Ny5jbGVyay5hY2NvdW50cy5kZXYk"
 
   if (!PUBLISHABLE_KEY) {
     throw new Error('Add your Clerk Publishable Key to the .env file')
   }
 
   const [expenses, setExpenses] = useState([]);
-  const [income, setIncome] = useState([]);
 
   // const navigate = useNavigate();
   // navigate('/');
