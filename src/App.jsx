98d631e4e75f28947031c80d7c7e51b678c4f09c
mash-xf <felix.macharia@student.moringaschool.com>
2026-05-17 19:47:56 +0300

    update react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,61 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { ClerkProvider } from '@clerk/react'
-import { BrowserRouter, Routes, Route } from 'react-router'
+// Note: Changed 'react-router' to 'react-router-dom' which is standard for web apps
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import Header from './components/Header'
-import { useNavigate } from "react-router";
 
-import header from './components/Header'
 import ExpensesForm from './components/ExpensesForm'
 import IncomeForm from './components/IncomeForm'
 import Home from './components/Home'
 import Login from './components/Login'
 
-const isProduction = true;
+// Create a wrapper component so we can use useNavigate() inside ClerkProvider
+function AppContent({ expenses, setExpenses, income, setIncome }) {
+  const navigate = useNavigate();
+
+  return (
+
+    <ClerkProvider 
+      publishableKey="pk_test_YWRhcHRpbmctaGFnZmlzaC00Ny5jbGVyay5hY2NvdW50cy5kZXYk" 
+      routerPush={(to) => navigate(to)}
+      routerReplace={(to) => navigate(to, { replace: true })}
+      afterSignInUrl="/expense-tracker-project/" 
+      afterSignOutUrl="/expense-tracker-project/"
+    >
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home
+          expenses={expenses}
+          setExpenses={setExpenses}
+          income={income}
+          setIncome={setIncome} 
+        />} />
+        <Route path="/add-expense" element={<ExpensesForm expenses={expenses}
+          setExpenses={setExpenses}
+          income={income}
+          setIncome={setIncome} />} />
+        <Route path="/add-income" element={<IncomeForm expenses={expenses}
+          setExpenses={setExpenses}
+          income={income}
+          setIncome={setIncome} />} />
+        <Route path="/login" element={<Login expenses={expenses}
+          setExpenses={setExpenses}
+          income={income}
+          setIncome={setIncome} />} />
+      </Routes>
+    </ClerkProvider>
+  );
+}
+
 function App() {
-
-
-  // const PUBLISHABLE_KEY = `pk_live_Y2xlcmsubWFzaC14Zi5naXRodWIuaW8k`
-  const PUBLISHABLE_KEY = "pk_test_YWRhcHRpbmctaGFnZmlzaC00Ny5jbGVyay5hY2NvdW50cy5kZXYk"
-
-  if (!PUBLISHABLE_KEY) {
-    throw new Error('Add your Clerk Publishable Key to the .env file')
-  }
-
   const [expenses, setExpenses] = useState([]);
   const [income, setIncome] = useState([]);
 
-  // const navigate = useNavigate();
-  // navigate('/');
   useEffect(() => {
     const fetchData = async () => {
       try {
         const expensesResponse = await fetch('https://6a08073efa9b27c848fa8a5f.mockapi.io/api/et/expenses');
-        // const expensesResponse = await fetch('http://localhost:3500/expenses');
         const expensesData = await expensesResponse.json();
         setExpenses(expensesData);
 
@@ -46,33 +70,15 @@ function App() {
     fetchData();
   }, []);
 
-
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} navigate={(to) => window.history.pushState(null, '', to)}>
-      <BrowserRouter basename="/expense-tracker-project">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home
-            expenses={expenses}
-            setExpenses={setExpenses}
-            income={income}
-            setIncome={setIncome} 
-          />} />
-          <Route path="/add-expense" element={<ExpensesForm expenses={expenses}
-            setExpenses={setExpenses}
-            income={income}
-            setIncome={setIncome} />} />
-          <Route path="/add-income" element={<IncomeForm expenses={expenses}
-            setExpenses={setExpenses}
-            income={income}
-            setIncome={setIncome} />} />
-          <Route path="/login" element={<Login expenses={expenses}
-            setExpenses={setExpenses}
-            income={income}
-            setIncome={setIncome} />} />
-        </Routes>
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter basename="/expense-tracker-project">
+      <AppContent 
+        expenses={expenses} 
+        setExpenses={setExpenses} 
+        income={income} 
+        setIncome={setIncome} 
+      />
+    </BrowserRouter>
   )
 }
 
